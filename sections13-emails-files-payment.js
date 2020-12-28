6d67f42580a790a7d47cac1f2994192db82bc3d2
kapil <kapil.kathuria@gmail.com>
2020-12-28 09:48:06 +0530

    section 13 - reviewed 4 email templates videos

---
+++ sections13-emails-files-payment.js
@@ -0,0 +1,8 @@
+// advanced features - emails, files upload and payment
+
+// emails
+// email-templates with pug and sendgrid service
+// see 4-natours/after-section-13/utils/email.js for class created to send email
+// templates are in 4-natours/after-section-13/views/email
+// to get the url of current app (in 4-natours/after-section-13/controllers/authController.js)
+//   const url = `${req.protocol}://${req.get('host')}/me`;
